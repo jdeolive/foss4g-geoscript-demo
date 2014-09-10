76b6fa98350cc87d712591d19ae96fa3811306af
jdeolive <jdeolive@boundlessgeo.com>
2014-09-09 17:18:36 -0700

    Some comments to classify_lands script

--- classify_areas.py
+++ classify_areas.py
@@ -13,20 +13,26 @@ from geoscript.geom import Geometry
   description='Classifies protected areas covering a specific area.'
 )
 def run(area):
+  # look up the protected areas layer
   padus = Layer('oregon:padus').data
 
+  # data structure for results
   total_area = 0
   results = {}
 
+  # look up intersecting features
   for f in padus.features('INTERSECTS(%s, %s)' % ('the_geom', area)):
+    # get the land type and calculate intersecting area
     land_type = f['Own_Type']
     land_area = f.geom.intersection(area).area
 
+    # update result table
     if land_type not in results:
       results[land_type] = 0;
 
     results[land_type] += land_area
     total_area += land_area
 
+  # calculate final land percentage and return result
   return {'result': 
     dict([(k,round(100 * v/total_area, 2)) for k,v in results.iteritems()])}
